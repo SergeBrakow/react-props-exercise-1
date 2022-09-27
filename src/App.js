4d4ef3fa98021552fa4893bd10812660acce7aa1
SergeBrakow <serg_@online.de>
2022-09-27 11:43:46 +0200

    first task

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ export default function App() {
   );
 }
 
-const HelloSomeone = () => {
-  return <div></div>;
+const HelloSomeone = ({name, greeting, hasDarkMode = true}) => {
+  return <div className={hasDarkMode ? "dark" : ""}>
+    <h2>{`${greeting} ${name === "Mario" ? "Boss" : name } `}</h2> 
+    </div>;
 };
